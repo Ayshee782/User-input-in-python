2d45f1f52db36d15e5f6e250c227294ec8a6ab96
Ayshee <152168245+Ayshee782@users.noreply.github.com>
2025-02-15 12:03:18 +0600

    Add files via upload

---
+++ user input.py
@@ -0,0 +1,8 @@
+name= input("enter name=") #get string 
+print("welcome ",name)
+
+a=int(input("enter num= ")) #get integer number
+print("get num=",a)
+
+value=float(input("enter some value= ")) #get float number
+print(type(value),value)
