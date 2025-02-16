df1be9a144b0651969d1c9ada1a7a70421c6d752
Ayshee <152168245+Ayshee782@users.noreply.github.com>
2025-02-16 22:22:03 +0600

    Add files via upload

---
+++ practice 4.py
@@ -0,0 +1,9 @@
+""" write a program to input 2 int number ,
+ a and b print true if a is grater then
+ or equal to b . if not print false."""
+
+a= int (input("enter num1 = "))
+b= int (input("enter num2 = "))
+val1 = (a>=b)
+val2 = (a==b)
+print("or using=",val1 or val2)
