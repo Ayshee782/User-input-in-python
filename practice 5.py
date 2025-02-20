d3afa8ca9075e13eabd399dbac65d055bcf00143
Ayshee <152168245+Ayshee782@users.noreply.github.com>
2025-02-20 22:50:40 +0600

    Add files via upload

---
+++ practice 5.py
@@ -0,0 +1,7 @@
+"""Prompt the user to enter their age 
+ and print a message indicating how 
+ many years until they turn 100."""
+age= int(input("enter your age= "))
+a=100
+b=a-age
+print("the age= ",b)
